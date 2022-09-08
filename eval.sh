38d8e946ea1ef359cd7e5688900804c1cfd3cbb8
Fred Brennan <copypaste@kittens.ph>
2022-09-08 13:16:43 -0400

    +[plugin] eval

---
+++ eval.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+Deval() {
+    if [ $# -eq 0 ]; then
+        >&2 echo "Must give an argument. 😉"
+        return 0;
+    fi
+    local BASH_SETTINGS="$-"
+    declare -gx __DEBUG_EVAL_SET
+    : "${__DEBUG_EVAL_SET:=-xv}"
+    set "$__DEBUG_EVAL_SET"
+    # Runs the command. As safe as the original command :^)
+    $@
+    for setting in x v; do
+        if [[ ! $setting =~ $BASH_SETTINGS ]]; then
+            set "+$setting"
+        fi
+    done
+}
